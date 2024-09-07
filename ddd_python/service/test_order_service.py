3935df1d278e59fcb1d7d161e9de5bca748f0279
Wagner Reis <wagnerreis1997@gmail.com>
2024-09-07 14:26:35 -0300

    test: add test for order service

---
+++ ddd_python/service/test_order_service.py
@@ -0,0 +1,16 @@
+from ddd_python.entity.order import Order
+from ddd_python.entity.order_item import OrderItem
+
+
+class TestOrderService:
+    def test_should_get_total_of_all_orders(self):
+        item1 = OrderItem("123", "456", "Item 1", 10, 1)
+        item2 = OrderItem("123asd", "456213", "Item 2", 20, 1)
+
+        order = Order("123", "456", [item1])
+        order2 = Order("123123", "45645", [item2])
+
+        total = OrderService.total([order, order2])
+
+        assert total == 20
+
