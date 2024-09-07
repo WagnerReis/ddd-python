a3e818c720479796383b4d96d88118fae8a58348
Wagner Reis <wagnerreis1997@gmail.com>
2024-09-07 14:45:24 -0300

    test: add test of reward points

--- ddd_python/service/test_order_service.py
+++ ddd_python/service/test_order_service.py
@@ -1,9 +1,19 @@
+from ddd_python.entity.customer import Customer
 from ddd_python.entity.order import Order
 from ddd_python.entity.order_item import OrderItem
+from ddd_python.entity.customer import Customer
 from ddd_python.service.order_service import OrderService
 
 
 class TestOrderService:
+    def test_should_place_an_order(self):
+        customer = Customer("c1", "customer 1")
+        item1 = OrderItem("123", "456", "Item 1", 10, 1)
+
+        order = OrderService.place_order(customer, [item1])
+
+        assert customer.get_reward_points() == 5
+
     def test_should_get_total_of_all_orders(self):
         item1 = OrderItem("123", "456", "Item 1", 10, 1)
         item2 = OrderItem("123asd", "456213", "Item 2", 20, 1)
