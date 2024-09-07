9e89919931ef4c9c4bdc1e2f5d927cb916a2fc8a
Wagner Reis <wagnerreis1997@gmail.com>
2024-09-07 09:30:05 -0300

    feat: add order validation

--- ddd_python/entity/order.py
+++ ddd_python/entity/order.py
@@ -5,10 +5,21 @@ from ddd_python.entity.order_item import OrderItem
 
 
 class Order:
-    def __init__(self, order_id: str, curtomer_id: str, items: List[OrderItem]):
+    def __init__(self, order_id: str, customer_id: str, items: List[OrderItem]):
         self._id = order_id
-        self._customer_id = curtomer_id
+        self._customer_id = customer_id
         self._items = items
+        self.validate()
+
+    def validate(self):
+        if not self._id:
+            raise ValueError("Id is required")
+
+        if not self._customer_id:
+            raise ValueError("Customer id is required")
+
+        if len(self._items) == 0:
+            raise ValueError("Order must have at least one item")
 
     def total(self):
         return functools.reduce(lambda a, b: a.get_price() + b.get_price(), self._items)
