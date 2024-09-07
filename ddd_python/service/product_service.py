0ee4d40b9e6f8e9d5d56e35140b2193f34af7d74
Wagner Reis <wagnerreis1997@gmail.com>
2024-09-07 14:06:59 -0300

    feat: add product service

---
+++ ddd_python/service/product_service.py
@@ -0,0 +1,10 @@
+from typing import List
+
+from ddd_python.entity.product import Product
+
+
+class ProductService:
+    @staticmethod
+    def increase_price(products: List[Product], percentage: float) -> List[Product]:
+        return [product.change_price((product.get_price() * percentage) / 100 + product.get_price()) or product for
+                product in products]
---
+++ ddd_python/service/test_product_service.py
@@ -0,0 +1,14 @@
+from ddd_python.entity.product import Product
+from ddd_python.service.product_service import ProductService
+
+
+class TestProductService:
+    def test_should_change_price_of_all_products(self):
+        product1 = Product("123", "Product 1", 10, 1)
+        product2 = Product("456", "Product 2", 20, 2)
+        products = [product1, product2]
+
+        ProductService.increase_price(products, 100)
+
+        assert product1.get_price() == 20
+        assert product2.get_price() == 40
